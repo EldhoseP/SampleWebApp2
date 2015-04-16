dda97e8f9dc53706d5f847655eba534d71b4f414
Stewart Pavitt <stewart.pavitt@miicard.com>
2015-04-16 16:40:23 +0100

    Added styling by Jack

--- ExampleSetup/Models/CredentialsModel.cs
+++ ExampleSetup/Models/CredentialsModel.cs
@@ -9,22 +9,22 @@ namespace ExampleSetup.Models
     public class CredentialsModel
     {
         [Required]
-        [Display(Name = "Please enter the API endpoint provided from Direct ID")]
+        [Display(Name = "API Endpoint")]
         public string API { get; set; }
         [Required]
-        [Display(Name = "Please enter the Client ID provided from Direct ID")]
+        [Display(Name = "Client ID")]
         public string ClientID { get; set; }
         [Required]
-        [Display(Name = "Please enter the key provided from Direct ID")]
+        [Display(Name = "Secret Key")]
         public String SecretKey { get; set; }
         [Required]
-        [Display(Name = "Please enter the Resource ID provided from Direct ID")]
+        [Display(Name = "Resource ID Endpoint")]
         public String ResourceID { get; set; }
         [Required]
-        [Display(Name = "Please enter the Version provided from Direct ID")]
+        [Display(Name = "API Version")]
         public String Version { get; set; }
         [Required]
-        [Display(Name = "Please enter the Authority provided from Direct ID")]
+        [Display(Name = "Authentication Context Path")]
         public String Authority { get; set; }
     }
 }
